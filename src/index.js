fc91ee02bc8de6fcdcd483a60a7df334a7587c45
Anko <totallynotankooo@gmail.com>
2023-08-15 22:53:41 +0700

    add models, routes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import { usersRouter } from "./routes/users.route.js";
 
+dotenv.config();
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
+app.use("/auth", usersRouter);
+
 app.get("/", (req, res) => {
   res.send("hi this from server");
+});
+
+mongoose.connect(process.env.MONGODB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
 app.listen(5000, () => console.log("server running on http://localhost:5000"));
